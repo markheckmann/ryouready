79d86bf6a1393399b3df7321977d255b4fb5046b
Mark Heckmann <mark.heckmann@gmx.de>
2012-04-07 10:53:44 +0200

    Adding basic info

---
+++ R/ryouready-package.r
@@ -0,0 +1,9 @@
+#' \pkg{ryouready}: an R package to accompany my R book
+#'
+#' @author    Mark Heckmann. 
+#' @keywords package ryouready
+#' @keywords internal
+#' @name ryouready
+#' @docType package
+#'
+NULL
