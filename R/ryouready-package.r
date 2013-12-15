d7eb7bf0a24dbd5cccf30e05692c46bb87672443
Mark Heckmann <heckmann.mark@gmail.com>
2013-12-15 13:13:09 +0100

    Modify package doc

--- R/ryouready-package.r
+++ R/ryouready-package.r
@@ -1,5 +1,11 @@
-#' \pkg{ryouready}: an R package to accompany the 'R you ready?' R book
+#' \pkg{ryouready}: An R package to accompany the 'R you ready?' R book
 #'
+#' This package contains several functions that make an R beginners 
+#' life easier and which are used in my forthcoming 'R you ready?' R book.
+#' 
+#' The book's website where you can find the fulltext freely available 
+#' currently is \url{http://ryouready.markheckmann.de}.
+#'  
 #' @author    Mark Heckmann. 
 #' @keywords package ryouready
 #' @keywords internal
